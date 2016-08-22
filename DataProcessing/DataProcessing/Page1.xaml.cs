2ef3e5dfb01be0011b056ee3f31584684aa5a788
toanhanduc <toanhanduc@gmail.com>
2016-08-22 20:20:05 +0700

    creat New & makeNavigation

---
+++ DataProcessing/DataProcessing/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataProcessing
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Page1_Button(object sender, RoutedEventArgs e)
+        {
+            Page2 P2 = new Page2();
+            this.NavigationService.Navigate(P2);
+        }
+    }
+}
