f3e83b9afe6780861ea8857d298fe51486196dc8
Hong <mrh95hn@gmail.com>
2016-08-27 18:10:16 +0700

    Hong update code cho button Browse and Start Search

--- DataProcessing/DataProcessing/thietlapHeSo.xaml.cs
+++ DataProcessing/DataProcessing/thietlapHeSo.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using Microsoft.Win32;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace DataProcessing
 {
@@ -23,6 +26,48 @@ namespace DataProcessing
         public thietlapHeSo()
         {
             InitializeComponent();
+        }
+
+        private void browseFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.DefaultExt = ".xls";
+
+            openfile.Filter = "(.xls)|*.xls|(.xlsx)|*.xlsx";
+            //openfile.ShowDialog();
+
+            var browsefile = openfile.ShowDialog();
+
+            if (browsefile == true)
+                txtFilePath.Text = openfile.FileName;
+        }
+
+        private void startSearch(object sender, RoutedEventArgs e)
+        {
+            Excel.Application excel;
+            excel = new Excel.Application();
+            Excel.Workbook WB = excel.Workbooks.Open(txtFilePath.Text);
+            WB = excel.ActiveWorkbook;
+            Excel.Worksheet WS;
+            WS = WB.ActiveSheet;
+            int start = Environment.TickCount;
+            for (int i = 2; i <= WS.UsedRange.Columns.Count; i++)
+            {
+
+                int temp = 0;
+                Excel.Range a = WS.get_Range((Excel.Range)WS.Cells[i][2], (Excel.Range)WS.Cells[i][WS.UsedRange.Rows.Count]);
+                object arr = a.Value;
+                foreach (object s in (Array)arr)
+                {
+                    string tmp = (string)s;
+                    if (tmp != "1")
+                        continue;
+                    else
+                        temp += int.Parse(tmp);
+                }
+
+            }
+            MessageBox.Show("Đọc hết: " + ((double)(Environment.TickCount - start) / 1000).ToString() + "ms");
         }
     }
 }
