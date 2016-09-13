fdb7abb392a7b0f2274f264da34b550c40bc1ef3
toanhanduc <toanhanduc@gmail.com>
2016-09-13 10:48:25 +0700

    More UI

--- DataProcessing/DataProcessing/FindingStatus.xaml.cs
+++ DataProcessing/DataProcessing/FindingStatus.xaml.cs
@@ -16,25 +16,13 @@ using System.Windows.Shapes;
 namespace DataProcessing
 {
     /// <summary>
-    /// Interaction logic for Welcome.xaml
+    /// Interaction logic for FindingStatus.xaml
     /// </summary>
-    public partial class Welcome : Page
+    public partial class FindingStatus : Page
     {
-        public Welcome()
+        public FindingStatus()
         {
             InitializeComponent();
-        }
-
-        private void gotothietlapHeso(object sender, RoutedEventArgs e)
-        {
-            thietlapHeSo tlhs = new thietlapHeSo();
-            this.NavigationService.Navigate(tlhs);
-        }
-
-        private void myGif_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            myGif.Position = new TimeSpan(0, 0, 1);
-            myGif.Play();
         }
     }
 }
